b87ab5ed69c774c1ab2acd6cb7a684bfbb67b269
Mykie-P <132044836+Mykie-P@users.noreply.github.com>
2023-05-30 06:36:31 +0800

    Update MainC.java

--- MainC.java
+++ MainC.java
@@ -1,13 +1,13 @@
 public class MainC {
     public static void main(String[] args) {
     	System.out.println("Car");
-    	ToyotaVios toyotaVios = new ToyotaVios(120, "Black", 700000.0, "Summer Tires");
+    	ToyotaVios toyotaVios = new ToyotaVios(60, "Black", 700000.0, "Winter Tires");
         toyotaVios.displayInfo();
         toyotaVios.drive();
         toyotaVios.stop();
         
     	System.out.println("\nPlane");
-        U2SpyPlane u2SpyPlane = new U2SpyPlane(500, "Gold", 10000000.0, 80.5);
+        U2SpyPlane u2SpyPlane = new U2SpyPlane(500, "Blue", 10000000.0, 30.5);
         u2SpyPlane.displayInfo();
         u2SpyPlane.fly();
         u2SpyPlane.stop();
